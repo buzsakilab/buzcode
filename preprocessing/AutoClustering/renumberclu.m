069ab3e6b73b5236df57a2daa9912b05bc835cda
David Tingley <davidtingley2@gmail.com>
2017-02-24 15:01:07 -0500

    object orienting

--- preprocessing/AutoClustering/renumberclu.m
+++ preprocessing/AutoClustering/renumberclu.m
@@ -1,6 +1,11 @@
-function clu=renumberclu(clu)
+function [clu log] =renumberclu(clu,log)
+% rewritten by David Tingley, 2017
 
-ix = unique(clu);
-for ii=1:length(ix)
-    clu(clu==ix(ii))=ii;
+cluster_names = unique(clu);
+for i=1:length(cluster_names)
+    if cluster_names(i) ~= 0
+    clu(find(clu==cluster_names(i))) = i;
+     log = [log sprintf('%d -> %d; reordering clusters\n',cluster_names(i),i)];
+    end
 end
+
