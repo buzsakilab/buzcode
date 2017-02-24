115049fd8fe3bcd10006a72ca994d30a2bea2cd3
David Tingley <davidtingley2@gmail.com>
2017-02-24 13:06:33 -0500

    'test'

--- preprocessing/AutoClustering/updateclu.m
+++ preprocessing/AutoClustering/updateclu.m
@@ -1,4 +1,11 @@
 function clu = updateclu(clu,ix,newclu)
+% updates identities in parts of a clu file
+
+%% INPUTS
+
+clu - N x 1 vector of cluster identities for each extracted waveform
+ix - M x 1 vector of indices where new identities will be assigned
+newclu - integer value for the new cluster identity
 
 for ii=1:length(ix)
     clu(clu==ix(ii))=newclu;
