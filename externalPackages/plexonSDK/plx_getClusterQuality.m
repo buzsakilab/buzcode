c9fe860add17599fde8ee3fd1e62a46e3dd14a4f
David Tingley <davidtingley2@gmail.com>
2017-03-09 13:59:04 -0500

    update

--- externalPackages/plexonSDK/plx_getClusterQuality.m
+++ externalPackages/plexonSDK/plx_getClusterQuality.m
@@ -29,43 +29,50 @@ else
     plx = varargin{1};    
 end
 
-[OpenedFileName, Version, Freq, Comment, Trodalness, NPW, PreTresh, ...
-    SpikePeakV, SpikeADResBits, SlowPeakV, SlowADResBits, Duration, ...
-    DateTime] = plx_information(plx);
-
-
-for i=1:Trodalness:48
-    for j=1:50  % ..if you're recording > 50 units per *trode, you may want to do some merging...
-    [n, npw, ts, wave{i,j}] = plx_waves_v(plx,i,j-1); % -1 includes 'unsorted spikes'
-    [n, npw, ts, wave{i+1,j}] = plx_waves_v(plx,i+1,j-1);
-    end
+if isempty(plx)
+    LRatio=nan;
+    IsolDist=nan;
+    ID=nan;
+    return 
 end
+try
+    [OpenedFileName, Version, Freq, Comment, Trodalness, NPW, PreTresh, ...
+        SpikePeakV, SpikeADResBits, SlowPeakV, SlowADResBits, Duration, ...
+        DateTime] = plx_information(plx);
 
-c=1;
-for j=1:Trodalness:48
-    w=[];id=[];
-    for i=1:50
-        if wave{j,i}~=-1
-            if size(wave{j,i},1) == size(wave{j+1,i},1)
-            w =[w; wave{j,i}, wave{j+1,i}];
-            id = [id;i*ones(size(wave{j,i},1),1)];
+
+    for i=1:Trodalness:48
+        for j=1:50  % ..if you're recording > 50 units per *trode, you may want to do some merging...
+        [n, npw, ts, wave{i,j}] = plx_waves_v(plx,i,j-1); % -1 includes 'unsorted spikes'
+        [n, npw, ts, wave{i+1,j}] = plx_waves_v(plx,i+1,j-1);
+        end
+    end
+
+    c=1;
+    for j=1:Trodalness:48
+        w=[];id=[];
+        for i=1:50
+            if wave{j,i}~=-1
+                if size(wave{j,i},1) == size(wave{j+1,i},1)
+                w =[w; wave{j,i}, wave{j+1,i}];
+                id = [id;i*ones(size(wave{j,i},1),1)];
+                end
             end
         end
-    end
-    for i=1:50
-        if ~isempty(find(id==i))
-       IsolDist(c) = IsolationDistance(w, find(id==i));
-       [L, LRatio(c), df]  = L_Ratio(w, find(id==i));
-       if i == 1
-           ID(c) = 0;
-       else
-           ID(c) = 1;
-       end
-       c=1+c;
+        for i=1:50
+            if ~isempty(find(id==i))
+           IsolDist(c) = IsolationDistance(w, find(id==i));
+           [L, LRatio(c), df]  = L_Ratio(w, find(id==i));
+           if i == 1
+               ID(c) = 0;
+           else
+               ID(c) = 1;
+           end
+           c=1+c;
+            end
         end
+        clear w id
     end
-    clear w id
 end
-
 
 
