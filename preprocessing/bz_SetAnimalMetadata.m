6efac4767f82dc06c319d0782acf613cff85f959
Brendon <brendon.watson@gmail.com>
2017-03-28 19:16:25 -0400

    Finishing working versions of SetAnimalMetadata and SetSessionMetadata

--- preprocessing/bz_SetAnimalMetadata.m
+++ preprocessing/bz_SetAnimalMetadata.m
@@ -1,4 +1,4 @@
-function AnimalMetaData = bz_SetAnimalMetadata(basepath,basename)
+function AnimalMetadata = bz_SetAnimalMetadata(basepath,basename)
 %MakeXMLFromProbeMaps - Generate a .xml file to accompany .dat files for a
 %recording in the neuroscope/klusters/ndmanager system.  Uses a library of
 %probe map layouts.
@@ -49,6 +49,17 @@ function AnimalMetaData = bz_SetAnimalMetadata(basepath,basename)
 % the Free Software Foundation; either version 3 of the License, or
 % (at your option) any later version.
 
+%% Defaults... will be overwritten by incoming data
+df.NumberOfChannels = 1;%this is corrected below
+df.SampleRate = 20000;
+df.BitsPerSample = 16;
+df.VoltageRange = 20;
+df.Amplification = 1000;
+df.LfpSampleRate = 1250;
+df.PointsPerWaveform = 32;
+df.PeakPointinWaveform = 16;
+df.FeaturesPerWave = 4;
+
 %% Variable parsing
 if ~exist('basepath','var')
     basepath = cd;
@@ -62,106 +73,112 @@ elseif isempty(basename)
 end
 
 %% Value setting - humans must do this
-AnimalMetaData.AnimalName = 'Ket1';
-AnimalMetaData.AnimalBaseFolderPath = '/Users/brendon/Dropbox/Data/Ket1';%this can be changed for each computer and can then act as a handle for all subsequent analyses
+AnimalMetadata.AnimalName = 'Ket1';
+AnimalMetadata.AnimalBasepath = '/balrog_zpool/Ket1';%this can be changed for each computer and can then act as a handle for all subsequent analyses
 
-AnimalMetaData.Species = 'Rat';
-AnimalMetaData.Strain = 'SpragueDawley';
-AnimalMetaData.GeneticLine = 'WildType';
-AnimalMetaData.Sex = 'male';
-AnimalMetaData.DateOfBirth = '20161221';%YYYYMMDD format
-AnimalMetaData.WeightGramsAtSurgery = 405;%grams
+AnimalMetadata.Species = 'Rat';
+AnimalMetadata.Strain = 'SpragueDawley';
+AnimalMetadata.GeneticLine = 'WildType';
+AnimalMetadata.Sex = 'male';
+AnimalMetadata.DateOfBirth = '20161221';%YYYYMMDD format
+AnimalMetadata.WeightGramsAtSurgery = 405;%grams
 
-AnimalMetaData.Surgery.Date = '20140317';
-AnimalMetaData.Surgery.Anesthesia.Name = 'Isoflurane';
-AnimalMetaData.Surgery.Anesthesia.ConcentrationPercent = '1';
-AnimalMetaData.Surgery.Analgesic.Name = 'Buprenex';
-AnimalMetaData.Surgery.Analgesic.Milligrams = 0.06;%usually given at 0.15mg/ml
-AnimalMetaData.Surgery.Antibiotics.Topical = 'Neopredef';
-AnimalMetaData.Surgery.Antibiotics.Intraperitoneal = '';
-AnimalMetaData.Surgery.Complications = '';
-AnimalMetaData.Surgery.DamageSites = '';
-AnimalMetaData.Surgery.Notes = 'Good';
+AnimalMetadata.Surgery.Date = '20140317';
+AnimalMetadata.Surgery.Anesthesia.Name = 'Isoflurane';
+AnimalMetadata.Surgery.Anesthesia.ConcentrationPercent = '1';
+AnimalMetadata.Surgery.Analgesic.Name = 'Buprenex';
+AnimalMetadata.Surgery.Analgesic.Milligrams = 0.06;%usually given at 0.15mg/ml
+AnimalMetadata.Surgery.Antibiotics.Topical = 'Neopredef';
+AnimalMetadata.Surgery.Antibiotics.Intraperitoneal = '';
+AnimalMetadata.Surgery.Complications = '';
+AnimalMetadata.Surgery.DamageSites = '';
+AnimalMetadata.Surgery.Notes = 'Good';
 
-AnimalMetaData.Virus.Strain = '';
-AnimalMetaData.Virus.Coordinates.Anteroposterior = [];%one for each injection
-AnimalMetaData.Virus.InjectionDate = '';
+AnimalMetadata.Virus.Strain = '';
+AnimalMetadata.Virus.Coordinates.Anteroposterior = [];%one for each injection
+AnimalMetadata.Virus.InjectionDate = '';
 
-AnimalMetaData.Probes.NumberOfProbes = 2;
-AnimalMetaData.Probes.TargetAnatomy = {'dCA1','mPFC'};
-AnimalMetaData.Probes.ImplantCoordinates.Anteroposterior = [-3.5,2.7];%one for each probe
-AnimalMetaData.Probes.ImplantCoordinates.Mediolateral = [2.5,0.3];
-AnimalMetaData.Probes.ImplantAngle.Anteroposterior = [0,0];%degrees of top anterior as sitting behind animal
-AnimalMetaData.Probes.ImplantAngle.Mediolateral = [0,10];%degrees clockwise as sitting behind animal
-AnimalMetaData.Probes.ImplantCoordinates.DepthFromSurface = [1.5,2];
-AnimalMetaData.Probes.OrientationOfProbe.FirstGroupRelativeToLastGroupClockwiseDegrees = [90,135];%assumes linear arrays
-AnimalMetaData.Probes.OrientationOfProbe.GroupOffsetsFromCenter_ApMlDv = [];%for non-linear arrangements: group x 3 coordinates for change from center
-AnimalMetaData.Probes.PluggingOrder = [2,1];% order will be represented in .xml, ie if intan splitter dicates
-AnimalMetaData.Probes.SiteSizesInUmSq = [160];%In square microns
-AnimalMetaData.Probes.SpikeGroupLayoutFilenames = {'NRX_Buzsaki64_5X12';'NRX_Buzsaki64_8X8'};%filenames in /buzcode/GeneralComputation/geometries
-AnimalMetaData.Channels.ImpedanceFilenames = {'Ket1_Impedances_5Shank.csv','Ket1_Impedances_8Shank.csv'};%Filenames in basepath
+AnimalMetadata.Probes.UmPerScrewTurn = [288 288];
+AnimalMetadata.Probes.NumberOfProbes = 2;
+AnimalMetadata.Probes.TargetRegions = {'dCA1','mPFC'};
+AnimalMetadata.Probes.ImplantCoordinates.Anteroposterior = [-3.5,2.7];%one for each probe
+AnimalMetadata.Probes.ImplantCoordinates.Mediolateral = [2.5,0.3];
+AnimalMetadata.Probes.ImplantAngle.Anteroposterior = [0,0];%degrees of top anterior as sitting behind animal
+AnimalMetadata.Probes.ImplantAngle.Mediolateral = [0,10];%degrees clockwise as sitting behind animal
+AnimalMetadata.Probes.ImplantCoordinates.DepthFromSurface = [1.5,2];
+AnimalMetadata.Probes.OrientationOfProbe.FirstGroupRelativeToLastGroupClockwiseDegrees = [90,135];%assumes linear arrays
+AnimalMetadata.Probes.OrientationOfProbe.GroupOffsetsFromCenter_ApMlDv = [];%for non-linear arrangements: group x 3 coordinates for change from center
+AnimalMetadata.Probes.PluggingOrder = [2,1];% order will be represented in .xml, ie if intan splitter dicates
+AnimalMetadata.Probes.SiteSizesInUmSq = [160];%In square microns
+AnimalMetadata.Probes.ProbeLayoutFilenames = {'NRX_Buzsaki64_5X12';'NRX_Buzsaki64_8X8'};%filenames in /buzcode/GeneralComputation/geometries
+AnimalMetadata.Channels.ImpedanceFilenames = {};%Filenames in basepath
 
 %% Automated after this point
-[PerGroupSuperficialToDeep,SpatialXY,NumChansPerProbe,GroupsPerChannel] = bz_ReadProbeMapFiles(AnimalMetaData.Probes.SpikeGroupLayoutFilenames);
-AnimalMetaData.Probes.NumGroupsPerProbe = sum(~cellfun(@isempty,PerGroupSuperficialToDeep),2);
-AnimalMetaData.Channels.NumChannelsTotal = sum(NumChansPerProbe);
-AnimalMetaData.Channels.NumChansPerProbe = NumChansPerProbe;%to do
-AnimalMetaData.Channels.ProbeSpikeGroupLayoutSuperficialToDeep = PerGroupSuperficialToDeep;%to do
-AnimalMetaData.Channels.ProbeXYLocations = SpatialXY;
-
+[PerGroupSuperficialToDeep,SpatialXY,NumChansPerProbe,GroupsPerChannel] = bz_ReadProbeMapFiles(AnimalMetadata.Probes.ProbeLayoutFilenames);
+AnimalMetadata.Probes.NumGroupsPerProbe = sum(~cellfun(@isempty,PerGroupSuperficialToDeep),2);
+AnimalMetadata.Probes.WithinProbeXYLocations = SpatialXY;
+AnimalMetadata.Probes.NumChansPerProbe = NumChansPerProbe;%to do
+AnimalMetadata.Probes.ProbeSpikeGroupLayoutSuperficialToDeep = PerGroupSuperficialToDeep;%to do
+AnimalMetadata.Channels.NumChannelsTotal = sum(NumChansPerProbe);
+    df.NumberOfChannels = AnimalMetadata.Channels.NumChannelsTotal;
 %make lookup tables for probe number and anatomy for each channel
-po = AnimalMetaData.Probes.PluggingOrder;
+po = AnimalMetadata.Probes.PluggingOrder;
 lut = [];
 lut_ap = [];
 glut = [];
 glut_ap = [];
 pglut = [];
 pglut_p = [];
-for pidx = 1:AnimalMetaData.Probes.NumberOfProbes
-    lut = cat(1,lut,pidx*ones(AnimalMetaData.Channels.NumChansPerProbe(pidx),1));
-    lut_ap = cat(1,lut_ap,po(pidx)*ones(AnimalMetaData.Channels.NumChansPerProbe(po(pidx)),1));
+for pidx = 1:AnimalMetadata.Probes.NumberOfProbes
+    lut = cat(1,lut,pidx*ones(AnimalMetadata.Probes.NumChansPerProbe(pidx),1));
+    lut_ap = cat(1,lut_ap,po(pidx)*ones(AnimalMetadata.Probes.NumChansPerProbe(po(pidx)),1));
     glut = cat(1,glut,GroupsPerChannel{pidx}+length(pglut));
     glut_ap = cat(1,glut_ap,GroupsPerChannel{po(pidx)});
-    pglut = cat(1,pglut,[1:AnimalMetaData.Probes.NumGroupsPerProbe(pidx)]'+length(pglut));
-    pglut_p = cat(1,pglut_p,pidx*ones(AnimalMetaData.Probes.NumGroupsPerProbe(pidx),1));
+    pglut = cat(1,pglut,[1:AnimalMetadata.Probes.NumGroupsPerProbe(pidx)]'+length(pglut));
+    pglut_p = cat(1,pglut_p,pidx*ones(AnimalMetadata.Probes.NumGroupsPerProbe(pidx),1));
 end
-AnimalMetaData.Probes.GroupsLookupTable = [pglut_p pglut];
-AnimalMetaData.Channels.ProbeLookupTable = [[1:AnimalMetaData.Channels.NumChannelsTotal]' lut];
-AnimalMetaData.Channels.GroupLookupTable = [[1:AnimalMetaData.Channels.NumChannelsTotal]' glut];
-AnimalMetaData.Channels.AnatomyLookupTable = AnimalMetaData.Probes.TargetAnatomy(lut)';
-AnimalMetaData.Channels.ProbeLookupTable_AsPlugged = [[1:AnimalMetaData.Channels.NumChannelsTotal]' lut_ap];
-AnimalMetaData.Channels.GroupLookupTable_AsPlugged = [[1:AnimalMetaData.Channels.NumChannelsTotal]' glut_ap];
-AnimalMetaData.Channels.AnatomyLookupTable_AsPlugged = AnimalMetaData.Probes.TargetAnatomy(lut_ap)';
+AnimalMetadata.Probes.ProbeToGroupLookupTable = [pglut_p pglut];
+AnimalMetadata.Channels.ChannelToProbeLookupTable = [[1:AnimalMetadata.Channels.NumChannelsTotal]' lut];
+AnimalMetadata.Channels.ChannelToGroupLookupTable = [[1:AnimalMetadata.Channels.NumChannelsTotal]' glut];
+AnimalMetadata.Channels.ChannelToAnatomyLookupTable = AnimalMetadata.Probes.TargetRegions(lut)';
+AnimalMetadata.Channels.ChannelToProbeLookupTable_AsPlugged = [[1:AnimalMetadata.Channels.NumChannelsTotal]' lut_ap];
+AnimalMetadata.Channels.ChannelToGroupLookupTable_AsPlugged = [[1:AnimalMetadata.Channels.NumChannelsTotal]' glut_ap];
+AnimalMetadata.Channels.ChannelToAnatomyLookupTable_AsPlugged = AnimalMetadata.Probes.TargetRegions(lut_ap)';
 
 %Get impedances per channel
-AnimalMetaData.Channels.ImpedanceByChannel = cell(AnimalMetaData.Probes.NumberOfProbes);
-for pidx = 1:length(AnimalMetaData.Channels.ImpedanceFilenames)
-    tf = fullfile(basepath,AnimalMetaData.Channels.ImpedanceFilenames{pidx});
-    txt = read_mixed_csv(tf);
-    numchans = size(txt,1)-1;
-%     channums = 1:numchans;
-    for cidx = 1:numchans
-        eloc = strfind(txt{cidx+1,5},'e');
-        n1 = str2num(txt{cidx+1,5}(1:eloc-1));
-        n2 = str2num(txt{cidx+1,5}(eloc+1:end));
-        imp(cidx) = n1*10^n2;
+if ~isempty(AnimalMetadata.Channels.ImpedanceFilenames)
+    AnimalMetadata.Channels.ImpedanceByChannel = cell(AnimalMetadata.Probes.NumberOfProbes);
+    for pidx = 1:length(AnimalMetadata.Channels.ImpedanceFilenames)
+        tf = fullfile(basepath,AnimalMetadata.Channels.ImpedanceFilenames{pidx});
+        txt = read_mixed_csv(tf);
+        numchans = size(txt,1)-1;
+    %     channums = 1:numchans;
+        for cidx = 1:numchans
+            eloc = strfind(txt{cidx+1,5},'e');
+            n1 = str2num(txt{cidx+1,5}(1:eloc-1));
+            n2 = str2num(txt{cidx+1,5}(eloc+1:end));
+            imp(cidx) = n1*10^n2;
+        end
+    %     AnimalMetaData.Probes.ImpedanceByChannel{pidx} = cat(2,channums',imp');
+        AnimalMetadata.Channels.ImpedanceByChannel{pidx} = imp';
     end
-%     AnimalMetaData.Probes.ImpedanceByChannel{pidx} = cat(2,channums',imp');
-    AnimalMetaData.Channels.ImpedanceByChannel{pidx} = imp';
 end
 
-%% someone should combine XY with probe orientation angle and implant 
+%% save defaults
+AnimalMetadata.RecordingParameterDefaults = df;
+
+%% someone should combine XY with probe orientation angle and implant
 %coordinates to map each site at day of implant
 
 %% Make XML for animal
-aname = AnimalMetaData.AnimalName;
-apath = AnimalMetaData.AnimalBaseFolderPath;
-pfiles = AnimalMetaData.Probes.SpikeGroupLayoutFilenames;
-plugord = AnimalMetaData.Probes.PluggingOrder;
-bz_MakeXMLFromProbeMaps(apath,aname,pfiles,plugord);
+aname = AnimalMetadata.AnimalName;
+apath = AnimalMetadata.AnimalBasepath;
+pfiles = AnimalMetadata.Probes.ProbeLayoutFilenames;
+plugord = AnimalMetadata.Probes.PluggingOrder;
+bz_MakeXMLFromProbeMaps(apath,aname,pfiles,plugord,df);
 
 %% Save
-save(fullfile(apath,[aname '.AnimalInfo.mat']),'AnimalMetaData')
+save(fullfile(apath,[aname '_AnimalMetadata.mat']),'AnimalMetadata')
 
 
 
