e6a9a2d0f2152e7b1abdc9f98c66bc5c2a58c419
DLevenstein <dan.levenstein@gmail.com>
2017-04-17 13:39:57 -0400

    bz_LFPFromDat can now take user or .xml input for nchannels etc, but prompts to make a SessionMetadata file

--- preprocessing/bz_LFPFromDat.m
+++ preprocessing/bz_LFPFromDat.m
@@ -10,18 +10,37 @@ elseif isempty(basepath)
     basepath = cd;
 end
 basename = bz_BasenameFromBasepath(basepath);
-
+metadataname = fullfile(basepath,[basename,'.SessionMetadata.mat']);
+xmlname = fullfile(basepath,[basename,'.xml']);
 
 %% Setup
-load(fullfile(basepath,[basename,'.SessionMetadata.mat']));
+if exist(metadataname,'file')
+    display('Getting info from SessionMetadata.mat')
+    load(metadataname);
+    
+    nchannels = SessionMetadata.ExtracellEphys.Parameters.NumberOfChannels;
+    oldsamplerate = SessionMetadata.ExtracellEphys.Parameters.SampleRate;
+    newsamplerate = SessionMetadata.ExtracellEphys.Parameters.LfpSampleRate;
+elseif exist(xmlname,'file')
+    display(['Getting info from .xml, ',...
+        'you might want to make a SessionMetadata.mat...'])
+    parameters = LoadParameters(xmlname);
+    
+    nchannels = parameters.nChannels;
+    oldsamplerate = parameters.rates.wideband;
+    newsamplerate = parameters.lfpSampleRate;
+else
+    display(['No SessionMetadata.mat file... resorting to user input,',...
+        'you might want to make a SessionMetadata.mat...'])
+    nchannels = input('How many channels in the file?');
+    oldsamplerate = input('.dat SampleRate?');
+    newsamplerate = input('Desired .lfp SampleRate?');
+end
 
 datname = fullfile(basepath,[basename '.dat']);
 lfpname = fullfile(basepath,[basename '.lfp']);
 
-nchannels = SessionMetadata.ExtracellEphys.Parameters.NumberOfChannels;
 
-oldsamplerate = SessionMetadata.ExtracellEphys.Parameters.SampleRate;
-newsamplerate = SessionMetadata.ExtracellEphys.Parameters.LfpSampleRate;
 [N,D] = rat(newsamplerate/oldsamplerate);
 
 %% resample one file into another
