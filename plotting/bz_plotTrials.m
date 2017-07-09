3fa0494d3e852f821750c6dcb36713d702c1b33a
David Tingley <davidtingley2@gmail.com>
2017-07-09 18:24:16 -0400

    maxcorr saving each condition iteration

--- plotting/bz_plotTrials.m
+++ plotting/bz_plotTrials.m
@@ -9,13 +9,11 @@ for tt = 1:length(unique(behavior.events.trialConditions))
     ff = find(behavior.events.trialConditions==tt);
     
     for t = 1:length(ff)
-        if t ~= 5 & t ~= 6
     scatter(behavior.events.trials{ff(t)}.x,behavior.events.trials{ff(t)}.y,'.k')
     hold on
     scatter(behavior.events.trials{ff(t)}.x(1),behavior.events.trials{ff(t)}.y(1),'.g')
     scatter(behavior.events.trials{ff(t)}.x(end),behavior.events.trials{ff(t)}.y(end),'.r')
     hold on
-        end
     end
 %     axis square
 title(['condition: ' num2str(tt) ', # of trials: ' num2str(length(ff))]);
