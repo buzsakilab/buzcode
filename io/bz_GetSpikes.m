f037c76a48e21c39f34d7a783a143086c3bb72c4
David Tingley <davidtingley2@gmail.com>
2017-09-21 16:20:36 -0400

    bz_getspikes bugfix

--- io/bz_GetSpikes.m
+++ io/bz_GetSpikes.m
@@ -318,10 +318,11 @@ numcells = length(spikes.UID);
 for cc = 1:numcells
     groups{cc}=spikes.UID(cc).*ones(size(spikes.times{cc}));
 end
-alltimes = cat(1,spikes.times{:}); groups = cat(1,groups{:}); %from cell to array
-[alltimes,sortidx] = sort(alltimes); groups = groups(sortidx); %sort both
-spikes.spindices = [alltimes groups];
-
+if numcells>0
+    alltimes = cat(1,spikes.times{:}); groups = cat(1,groups{:}); %from cell to array
+    [alltimes,sortidx] = sort(alltimes); groups = groups(sortidx); %sort both
+    spikes.spindices = [alltimes groups];
+end
 
 
 
