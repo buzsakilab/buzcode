a4b76e2a44bf8584a085e6927be07803e0bcf84f
dlevenstein <dan.levenstein@gmail.com>
2017-09-21 16:19:25 -0400

    Merged bz_isEvents bug

--- utilities/bz_isEvents.m
+++ utilities/bz_isEvents.m
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 function [isEvents] = bz_isEvents(events)
-=======
-function [isEvent] = bz_isEvents(event)
->>>>>>> dev
 % USAGE
 % [isEvent] = bz_isEvents(event)
 % 
@@ -21,7 +17,6 @@ function [isEvent] = bz_isEvents(event)
 % written by david tingley, 2017
 
 
-<<<<<<< HEAD
 if isfield(events,'detectorinfo') && isfield(events,'timestamps') % check that fields exist
      if isstruct(events.detectorinfo) && isvector(events.timestamps) 
          % check that sub fields exist
@@ -30,31 +25,13 @@ if isfield(events,'detectorinfo') && isfield(events,'timestamps') % check that f
             isEvents = true;
          else
              isEvents = false;
-=======
-if isfield(event,'detectorinfo') && isfield(event,'timestamps') % check that fields exist
-     if isstruct(event.detectorinfo) && isvector(event.timestamps) 
-         % check that sub fields exist
-         if isfield(event.detectorinfo,'detectorname') && isfield(event.detectorinfo,'detectionparms') ...
-                 && isfield(event.detectorinfo,'detectioninterval') && isfield(event.detectorinfo,'detectiondate')
-            isEvent = true;
-         else
-             isEvent = false;
->>>>>>> dev
          end
          
      else
          warning('one of the required fields for an event type is not formatted correctly')
-<<<<<<< HEAD
          isEvents = false;
      end
 else
     warning('one of the required fields for an event type does not exist')
     isEvents = false;
-=======
-         isEvent = false;
-     end
-else
-    warning('one of the required fields for an event type does not exist')
-    isEvent = false;
->>>>>>> dev
 end
