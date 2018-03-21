1bf828e822ceedf53d943e1475a58a240c08f072
David Tingley <davidtingley2@gmail.com>
2018-03-21 18:31:44 -0400

    isbuzcode working, returns paths

--- utilities/bz_isBuzcode.m
+++ utilities/bz_isBuzcode.m
@@ -1,4 +1,4 @@
-function [isBuzcode] = bz_isBuzcode(events)
+function [isBuzcode, path] = bz_isBuzcode(varargin)
 % USAGE
 % [isBuzcode] = bz_isBuzcode(event)
 % 
@@ -10,11 +10,19 @@ function [isBuzcode] = bz_isBuzcode(events)
 %
 % written by david tingley, 2017
 
+if nargin == 0
+    path = pwd;
+else
+    path = varargin{1};
+end
+if bz_isSession % only worth continuing if it's a session..
+    sessionInfo = bz_getSessionInfo;
 
-sessionInfo = bz_getSessionInfo;
-
-if bz_isSessionInfo(sessionInfo) 
-    isBuzcode = 1;  % we should also add checks for dat/clu/res/wav files..
+    if bz_isSessionInfo(sessionInfo) 
+        isBuzcode = 1;  % we should also add checks for dat/clu/res/wav files..
+    else
+        isBuzcode = 0;
+    end
 else
     isBuzcode = 0;
 end
