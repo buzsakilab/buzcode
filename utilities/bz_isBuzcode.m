656edf88ef4e9d1a0a9b2bf4c26b6358824c3819
Dan Levenstein <dan.levenstein@gmail.com>
2018-03-22 11:13:06 -0400

    Update bz_isBuzcode.m
    updated so basePath input is clear

--- utilities/bz_isBuzcode.m
+++ utilities/bz_isBuzcode.m
@@ -1,22 +1,21 @@
-function [isBuzcode, path] = bz_isBuzcode(varargin)
+function [isBuzcode, path] = bz_isBuzcode(basePath)
 % USAGE
-% [isBuzcode] = bz_isBuzcode(event)
+% [isBuzcode] = bz_isBuzcode(basePath)
 % 
 % INPUT
-%       
+%       basePath    the basePath to check (default: current directory)
 %
 % OUTPUT
 %      
 %
 % written by david tingley, 2017
 
-if nargin == 0
-    path = pwd;
-else
-    path = varargin{1};
+if ~exist('basePath','var')
+    basePath = pwd;
 end
+
 if bz_isSession % only worth continuing if it's a session..
-    sessionInfo = bz_getSessionInfo(path,'noprompts',true);
+    sessionInfo = bz_getSessionInfo(basePath,'noprompts',true);
 
     if bz_isSessionInfo(sessionInfo) 
         isBuzcode = 1;  % we should also add checks for dat/clu/res/wav files..
