ee2726565aafd40f8d99dbcadc4461275d6298c2
AntonioFR <antferrui@gmail.com>
2018-07-22 15:28:07 -0400

    new bz_CSD function
    I just wrote this following the conventio of the other LFP analysis functions already in buzcode.
    This works nice for specified time segments. I will make another one for event-triggered CSD

--- analysis/lfp/bz_CSD.m
+++ analysis/lfp/bz_CSD.m
@@ -1,19 +1,34 @@
-function [ CSD ] = bz_CSD (lfp, channels, win, spat_sm, temp_sm, doDetrend, plotCSD, plotLFP)
 
-% [ CSD ] = bz_CSD (lfp, twin, spat_sm, temp_sm,doPlot)
+function [ csd ] = bz_CSD (lfp, varargin)
+
+% [ CSD ] = bz_CSD (lfp, varargin)
+% Calculates the 1D approximation of current source density (CSD) from a
+% linear array of LFPs
+
+% INPUT
+%    lfp            a buzcode structure with fields lfp.data,
+%                                                   lfp.timestamps
+%                                                   lfp.samplingRate
+%                   -lfp can also be a [t x 1] timeseries signal. in which
+%                   case you need to input 'samplingRate'
+%    <options>      optional list of property-value pairs (see table below)
 %
-% INPUTS:
-%       LFP         LFP structure in buzcode format:
-%                   a structure with fields lfp.data, lfp.timestamps
+%    =========================================================================
+%     Properties    Values
+%    -------------------------------------------------------------------------
 %       channels    vector with channels to inlcude. If empty take all (default)
 %       win         time interval to compute CSD. If empty take all (default)
 %       spat_sm     degree of spatial smoothing. Default = 0.
 %       temp_sm     degree of temporal smoothing. Default = 0.
-%       plotCSD      true/false. Default true.
+%       plotCSD     true/false. Default true.
+%    =========================================================================
 
 % OUTPUT:
-%       CSD         LFP structure in buzcode format
-%                    a structure with fields lfp.data, lfp.timestamps
+%    CSD            a buzcode structure with fields csd.data,
+%                                                   csd.timestamps
+%                                                   csd.samplingRate
+%                                                   csd.channels 
+%                                                   csd.params
 
 % Antonio FR, 7/18
 
@@ -21,6 +36,7 @@ function [ CSD ] = bz_CSD (lfp, channels, win, spat_sm, temp_sm, doDetrend, plot
 
 p = inputParser;
 addParameter(p,'channels',1:size(lfp.data,2),@isvector);
+addParameter(p,'samplingRate',1250,@isnumeric);
 addParameter(p,'win',[1 size(lfp.data,1)],@isnumeric);
 addParameter(p,'spat_sm',11,@isnumeric);
 addParameter(p,'temp_sm',11,@isnumeric);
@@ -28,92 +44,104 @@ addParameter(p,'doDetrend',false,@islogical);
 addParameter(p,'plotCSD',true,@islogical);
 addParameter(p,'plotLFP',true,@islogical);
 
-parse(p);
+parse(p,varargin{:});
+channels = p.Results.channels;
+samplingRate = p.Results.samplingRate;
+spat_sm = p.Results.spat_sm;
+temp_sm = p.Results.temp_sm;
+doDetrend = p.Results.doDetrend;
+plotCSD = p.Results.plotCSD;
+plotLFP = p.Results.plotLFP;
+
+%lfp input
+if isstruct(lfp)
+    data = lfp.data;
+    timestamps = lfp.timestamps;
+    samplingRate = lfp.samplingRate;
+elseif iscell(lfp) %for multiple trials
+    celllengths = cellfun(@length,lfp);
+    data = vertcat(lfp{:});
+elseif isnumeric(lfp)
+    data = lfp;
+    timestamps = [1:length(lfp)]'./samplingRate;
+end
+
+win = p.Results.win*samplingRate;
+
 
 %% Compute CSD
 
-lfp_frag = lfp.data(p.Results.win(1):p.Results.win(2),p.Results.channels);
+lfp_frag = data(win(1):win(2),channels);
 
 % detrend
-if p.Results.doDetrend
+if doDetrend
    lfp_frag = detrend(lfp_frag')';
 end
     
-% spatial smoothing
-if p.Results.spat_sm > 0
+% temporal smoothing
+if temp_sm > 0
    for ch = 1:size(lfp_frag,2) 
-       lfp_frag(:,ch) = smooth(lfp_frag(:,ch),p.Results.spat_sm,'sgolay');
+       lfp_frag(:,ch) = smooth(lfp_frag(:,ch),temp_sm,'sgolay');
    end
 end
 
-% temporal smoothing
-if p.Results.temp_sm > 0
+% spatial smoothing
+if spat_sm > 0
    for t = 1:size(lfp_frag,1) 
-       lfp_frag(t,:) = smooth(lfp_frag(t,:),p.Results.temp_sm,'sgolay');
+       lfp_frag(t,:) = smooth(lfp_frag(t,:),spat_sm,'lowess');
    end
 end
 
 % calculate CSD 
-
 CSD = diff(lfp_frag,2,2);
+
+% generate output structure
+csd.data = CSD;
+csd.timestamps = timestamps(win(1):win(2));
+csd.samplingRate = samplingRate;
+csd.channels = channels; 
+csd.params.spat_sm = spat_sm;
+csd.params.temp_sm = temp_sm;
+csd.params.detrend = doDetrend;
 
 %% Plot
 
-if plotCSD
-   cmax = max(max(CSD)); 
-   
-   figure;
-   contourf(lfp.timestamps,1:size(CSD,2),CSD',40,'LineColor','none');hold on;
-   colormap jet; caxis([-cmax cmax]);
-   set(gca,'YDir','reverse');xlabel('time (s)');ylabel('channel');title(CSD); 
-   
-end
-
 if plotLFP
     
+    cmax = max(max(CSD)); 
+
     figure;
     subplot(1,2,1);
-    contourf(lfp.timestamps,1:size(CSD,2),CSD',40,'LineColor','none');hold on;
+    contourf(timestamps(win(1):win(2)),1:size(CSD,2),CSD',40,'LineColor','none');hold on;
     colormap jet; caxis([-cmax cmax]);
     set(gca,'YDir','reverse');xlabel('time (s)');ylabel('channel');title('CSD'); 
    
     subplot(1,2,2);
-    plotLFPprofile(lfp_frag',scale)
-    set(gca,'YDir','reverse','YTickLabel',[]);xlabel('time (s)');ylabel('channel');title('LFP');   
-    
-end
-
-
-%%
-function plotLFPprofile( lfp,scale,varargin )
-%   plotLFPprofile( lfp,scale,varargin )
-%   plot vertical profile of LFP from a single shank
-%   lfp = Nchannels x Nsamples
-%   scale = multiplying factor for plot LFP
-%
-%   Azahara and Antonio, 2015
-
-    nargin = length(varargin);
-
-    for i =1:nargin
-    if strcmp(varargin{i},'DoDetrend')
-           lfp = detrend(lfp);
-    end
-    end
-
-    %figure;   
-    for i=1:size(lfp,1)
-        offset = 500*(i-1);
-        sh_tmp = scale*lfp(i,:) + offset;
-        plot(sh_tmp,'k','LineWidth',2); hold on;
+    for ch=1:size(lfp_frag,2)
+        offset = 500*(ch-1);
+        sh_tmp = 10e5*(lfp_frag(:,ch)) + offset;
+        plot(timestamps(win(1):win(2)),sh_tmp,'k','LineWidth',1.5); hold on;
         clear sh_tmp
     end
+    set(gca,'YDir','reverse','YTickLabel',[]);ylim([-500 offset+500]);xlim([timestamps(win(1)) timestamps(win(2))]);
+    xlabel('time (s)');ylabel('channel');title('LFP');   
     
-    for i =1:nargin
-        if strcmp(varargin{i},'NoLabels')
-           set(gca,'box','off');axis off;
-        end
-    end
+elseif plotCSD  
+    
+     cmax = max(max(CSD)); 
+   
+     figure;
+     contourf(timestamps(win(1):win(2)),1:size(CSD,2),CSD',40,'LineColor','none');hold on;
+     colormap jet; caxis([-cmax cmax]);
+     set(gca,'YDir','reverse');xlabel('time (s)');ylabel('channel');title(CSD); 
+   
+end
 
 end
+
+
+    
+
+
+
 
