d6cb84810284a08ffe7e3921d93281c6975b1806
David Tingley <davidtingley2@gmail.com>
2018-09-27 15:33:07 -0400

    reduce memory usage

--- detectors/detectEvents/bz_FindPopBursts.m
+++ detectors/detectEvents/bz_FindPopBursts.m
@@ -20,7 +20,7 @@ function [popBursts] = bz_FindPopBursts(varargin)
 % -param input parser
 % - smoothing binSize
 % -% get start/stop times 
-%
+%-add detector info and params
 %
 % this function detects population burst events as described in Skaggs et
 % al XXXX
@@ -43,16 +43,18 @@ threshold = p.Results.threshold;
 durations = p.Results.durations;
 binSize = p.Results.binSize;
 smoothingWindow = p.Results.smoothingWindow;
+saveMat = p.Results.saveMat;
 
 %%
 
 spkMat = bz_SpktToSpkmat(spikes,'binSize',binSize,'overlap',1);
 
+% save memory and add to single vector...
+popRate = zeros(length(spkMat.data(:,1)),1);
 for spk = 1:length(spikes.times)
-    spkMat.zscoredData(:,spk) = zscore(Smooth(spkMat.data(:,spk),smoothingWindow));
+    popRate = popRate + zscore(Smooth(spkMat.data(:,spk),smoothingWindow));
 end
-
-popRate = nanmean(spkMat.zscoredData');
+popRate = popRate./spk;
 
 [pks locs width] = findpeaks(popRate,'minpeakheight',mean(popRate)+threshold*std(popRate),...
     'WidthReference','halfprom');
@@ -64,13 +66,24 @@ width = width(keep);
 
 exclude=[];
 for event = 1:length(pks)
-    forward = find(popRate(locs(event):locs(event)+durations(2))<0,1,'first');
-    back = durations(2) - find(popRate(locs(event)-durations(2):locs(event))<0,1,'last');
-    if ~isempty(back) & ~isempty(forward)
+    if locs(event)-durations(2) > 0
+        back = durations(2) - find(popRate(locs(event)-durations(2):locs(event))<0,1,'last');
+    else
+        back = durations(2) - find(popRate(1:locs(event))<0,1,'last');
+    end
+    if locs(event)+durations(2) <= length(popRate)
+        forward = find(popRate(locs(event):locs(event)+durations(2))<0,1,'first');
+    else
+        forward = find(popRate(locs(event):end)<0,1,'first');
+    end
+   
+    if ~isempty(back) & ~isempty(forward) & back < locs(event) 
         starts(event) = locs(event)-back;
         stops(event) = locs(event)+forward;
     else
         exclude = [exclude;event];
+        starts(event) = nan;
+        stops(event) = nan;
     end
 end
 
@@ -84,20 +97,19 @@ popBursts.sessionName = spikes.sessionName;
 popBursts.timestamps = [spkMat.timestamps(starts) spkMat.timestamps(stops)];
 popBursts.bursts = spkMat.timestamps(locs);
 popBursts.width = [stops-starts]';
-
+popBursts.amplitudes = pks;
+for event = 1:length(popBursts.amplitudes)
+    popBursts.nSpikes(event) = sum(sum(spkMat.data(starts(event):stops(event),:)));
+    popBursts.meanSpikes(event) = mean(mean(spkMat.data(starts(event):stops(event),:)));
+end
+popBursts.nSpikes = popBursts.nSpikes';
+popBursts.meanSpikes = popBursts.meanSpikes';
 
 
 
 if saveMat
     save([popBursts.sessionName '.popBursts.events.mat'],'popBursts')
 end
-c
-
-
-
-
-
-
 
 
 
