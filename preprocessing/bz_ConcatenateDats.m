bfccba30998d22b831ae136e0b7b27dcbd784d70
AntonioFR8 <antferrui@gmail.comn>
2018-10-11 17:10:55 -0400

    fix

--- preprocessing/bz_ConcatenateDats.m
+++ preprocessing/bz_ConcatenateDats.m
@@ -76,9 +76,8 @@ end
 
 
 %% If the dats are already merged quit
-if exist(fullfile(basepath,[basename,'.dat']),'file') && ...
-   exist(fullfile(basepath,[basename '.filesT.mat']),'file')
-    disp('.dat and filesT already exists in session directory')
+if exist(fullfile(basepath,[basename,'.dat']),'file')
+    disp('.dat already exists in session directory, not merging subdats')
     return
 end
 
@@ -185,35 +184,6 @@ if sortFiles
 %     fclose(fid);
 end
 
-%% save times of each individual file concatenated
-if exist(fullfile(basepath,[basename '.sessioninfo','.mat']),'file')
-    load(fullfile(basepath,[basename '.sessioninfo','.mat']));
-    fs = sessionInfo.rates.wideband;
-    nChannels = sessionInfo.nChannels;
-elseif exist(fullfile(basepath,[basename,'.xml']),'file')
-    [sessionInfo] = bz_getSessionInfo(basepath, 'noPrompts', false);
-    fs = sessionInfo.rates.wideband;
-    nChannels = sessionInfo.nChannels;
-end
-
-filesT.times(1) = recordingbytes(1)/(2*nChannels*fs);
-for d = 2:length(recordingbytes)
-    filesT.times(d,1) = filesT.times(d-1)+recordingbytes(d)/(2*nChannels*fs);
-end
-filesT.int(1,1) = 0; filesT.int(1,2) = filesT.times(1);
-for d = 2:length(recordingbytes)
-    filesT.int(d,1) = filesT.times(d-1);
-    filesT.int(d,2) = filesT.times(d);
-end
-save(fullfile(basepath,[basename '.filesT.mat']),'filesT');
-
-%% If the dats are already merged quit
-if exist(fullfile(basepath,[basename,'.dat']),'file') && ...
-   exist(fullfile(basepath,[basename '.filesT.mat']),'file')
-    disp('.dat already exists in session directory, calculating only filesT')
-    return
-end
-
 %% Concatenate
 %     cs = strjoin(datpaths.amplifier);
 %     catstring = ['! cat ', cs, ' > ',fullfile(basepath,[basename,'.dat'])];
@@ -251,13 +221,8 @@ eval(catstring)%execute concatention
 %     end
 % 
 %     save(fullfile(basepath,[basename '_DatInfo.mat']),'recordingbytes','recordingnames')
-<<<<<<< HEAD
 t = dir(newdatpath);
 if t.bytes ~= sum(datsizes.amplifier)
-=======
-%t = dir(newdatpath);
-if t.bytes ~= sum(recordingbytes)
->>>>>>> origin/dev
     error('New .dat size not right.  Exiting')
     return
 else
@@ -265,7 +230,6 @@ else
     disp('Primary .dats concatenated and size checked')
 end
 
-<<<<<<< HEAD
 %% save times of each individual file concatenated
 %moved to events.mat
 % filesT(1) = datsizes.amplifier(1)/2;
@@ -276,8 +240,6 @@ end
 % 
 % save(fullfile(basepath,'filesT'),'filesT');
 
-=======
->>>>>>> origin/dev
 %% Also concatenate the other .dats
 disp('Concatenating Other Dats..... continue to be patient')
 for odidx = 1:length(otherdattypes)
