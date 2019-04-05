507b1ae40964f79456e7bc7d7212a3b52572677b
Brendon Watson <brendon.watson@gmail.com>
2019-04-05 12:42:28 -0400

    Metadata tweaks
    Bug fixes

--- preprocessing/bz_ReadProbeGeometryFiles.m
+++ preprocessing/bz_ReadProbeGeometryFiles.m
@@ -1,11 +1,11 @@
 function [ChannelsPerGroupSuperficialToDeep,SpatialChannelXY,NumChansPerProbe,GroupsPerChannel] =...
-    bz_ReadProbeGeometryFiles(probemaplist)
+    bz_ReadProbeGeometryFiles(probemaplist,extrachannelgroups)
 % USAGE
 % [ChannelsPerGroupSuperficialToDeep,SpatialChannelXY,NumChansPerProbe,GroupsPerChannel] =...
 %     bz_ReadProbeGeometryFiles(probemaplist)
 % 
 % INPUTS
-%       probemaplist      - character cell (cell array of strings), each
+%    probemaplist      - character cell (cell array of strings), each
 %           string is the name of a probemap .xlsx file that is on the
 %           matlab path.  these are typically at
 %           /buzcode/generalComputation/gemetries
@@ -20,6 +20,14 @@ function [ChannelsPerGroupSuperficialToDeep,SpatialChannelXY,NumChansPerProbe,Gr
 %                   See NRX_Buzsaki32_4X8.xlsx or NRX_Buzsaki64_6X10.xlsx
 %                   as templates
 %
+%    extrachannelgroups  
+%                   For groups of channels to be added to array but not 
+%                   in probes/maps... ie for behavioral or opto stim 
+%                   monitoring channels.  Format: Vector of numbers of
+%                   channels to added to new groups at the end of the
+%                   probe-based groups.  So an input of [3,5] will make one
+%                   additional electrode group of 3 channels and a second
+%                   extra of group composed of the last 5 channels.
 %
 % OUTPUTS
 %       ChannelsPerGroupSuperficialToDeep - Cell array of cell arrays, 
@@ -61,9 +69,15 @@ function [ChannelsPerGroupSuperficialToDeep,SpatialChannelXY,NumChansPerProbe,Gr
 % Brendon Watson 2017                   
 
 
+if ~exist('extrachannelgroups','var')
+    extrachannelgroups = 0;
+end
+extrachannelgroups = extrachannelgroups(:)';%ensuring shape
+
 % grouplist_all = [];
 % groupchans_all = [];
-% channelcountoffset = 0;
+channelcountoffset = 0;
+groupcountoffset = 0;
 for pmidx = 1:length(probemaplist)
     tpf = probemaplist{pmidx};
     if ~strcmp(probemaplist{pmidx}(end-4:end),'.xlsx')
@@ -79,7 +93,10 @@ for pmidx = 1:length(probemaplist)
     for ridx = 1:length(groupdenoterows);
        groupperchannel = cat(1,groupperchannel,str2num(tptxt{groupdenoterows(ridx),groupcolumn}(7:end))); 
     end
-    grouplist_byprobe{pmidx} = unique(groupperchannel);
+    tgroups = unique(groupperchannel);
+    tnumgroups = length(tgroups);
+    
+    grouplist_byprobe{pmidx} = 1:length(tgroups)+groupcountoffset;
 %     grouplist_all = cat(1,grouplist_all,unique(groupperchannel));
     
     %find channels
@@ -92,13 +109,13 @@ for pmidx = 1:length(probemaplist)
     end
     
     %for each group, find the channels in it, save in sequence
-    for gidx = 1:length(grouplist_byprobe{pmidx})
-       tgidx = groupperchannel==grouplist_byprobe{pmidx}(gidx);%find rows whith this group/shank denotation 
-       ChannelsPerGroupSuperficialToDeep{pmidx,gidx} = channelnums(tgidx);
+    for gidx = 1:length(tgroups)
+       tgidx = groupperchannel==tgroups(gidx);%find rows whith this group/shank denotation 
+       ChannelsPerGroupSuperficialToDeep{pmidx,gidx} = channelnums(tgidx)+channelcountoffset;
 %        groupchans_all = cat(1,groupchans_all,channelnums(tgidx)); 
     end
     NumChansPerProbe(pmidx) = length(channelnums);
-%     channelcountoffset = NumChansPerProbe(pmidx);
+    channelcountoffset = channelcountoffset + NumChansPerProbe(pmidx);
 %     numchans = length(groupchans_all);
 
 %% Get XY information per channel
@@ -116,7 +133,27 @@ for pmidx = 1:length(probemaplist)
         end
         SpatialChannelXY{pmidx} = [channelnums X Y];
     end
-    GroupsPerChannel{pmidx} = groupperchannel(channelnums+1);
+    GroupsPerChannel{pmidx} = groupperchannel(channelnums+1)+groupcountoffset;
+    groupcountoffset = groupcountoffset + tnumgroups;%To have offset for next cycle
+
     clear channelnums X Y
 end
+
+%% Adding extra channels after probe channels - if any
+if ~isempty(extrachannelgroups)
+    if extrachannelgroups(1)>0
+        for eidx = 1:length(extrachannelgroups)
+            tchannels = (0:extrachannelgroups(eidx)-1)'+channelcountoffset;
+            
+            ChannelsPerGroupSuperficialToDeep{end+1,1} = (0:extrachannelgroups(eidx)-1)'+channelcountoffset;
+            channelcountoffset = channelcountoffset + extrachannelgroups(eidx);
+            
+            SpatialChannelXY{end+1} = cat(2,tchannels,nan(extrachannelgroups(eidx),2));
+            
+            GroupsPerChannel{end+1} = groupcountoffset+eidx*ones(extrachannelgroups(eidx),1);
+        end
+        NumChansPerProbe = cat(2,NumChansPerProbe,extrachannelgroups);
+    end
+end
+        
 1;
