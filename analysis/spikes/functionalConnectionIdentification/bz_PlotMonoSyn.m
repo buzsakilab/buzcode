8bffd9fc613f2c0856b35787850c0e3742e49af7
petersenpeter <petersen.peter@gmail.com>
2019-08-14 17:16:45 -0400

    Update bz_PlotMonoSyn.m

--- analysis/spikes/functionalConnectionIdentification/bz_PlotMonoSyn.m
+++ analysis/spikes/functionalConnectionIdentification/bz_PlotMonoSyn.m
@@ -1,8 +1,10 @@
 function keep_con = bz_PlotMonoSyn(ccgR,sig_con,Pred,Bounds,completeIndex,binSize,duration)
-% Manual sorting
+% Manual curating detected CCGs
 % click to deselect the ccg (turns pink)
 
 % Edited by Peter Petersen
+% petersen.peter@gmail.com
+% Last edited: 12-07-2019
 
 keep_con = sig_con;
 window  =false(size(ccgR,1),1);
@@ -15,15 +17,23 @@ allcel = unique(sig_con(:));
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
 h = figure('KeyReleaseFcn', {@keyPress},'Name','MonoSynCon inspector','NumberTitle','off','renderer','opengl');
+% p = uipanel(h,'Position',[0 0 1 .1],'BorderType','none')
+% p2 = uipanel(h,'Position',[0 0 0.01 0.01],'BorderType','none')
+uicontrol('Parent',h,'Style','pushbutton','Position',[5 405 20 15],'Units','normalized','String','<','Callback',@(src,evnt)goBack,'KeyPressFcn', {@keyPress});
+uicontrol('Parent',h,'Style','pushbutton','Position',[540 405 20 15],'Units','normalized','String','>','Callback',@(src,evnt)advance,'KeyPressFcn', {@keyPress});
+plotTitle = uicontrol('Parent',h,'Style','text','Position',[130 410 350 10],'Units','normalized','String','','HorizontalAlignment','center','FontSize',13);
 
-for i=1:length(allcel)
+i = 1;
+while i > 0 && i <= length(allcel)
     if ~ishandle(h)
         return
     end
-    cla, clf
+    delete(findobj(h, 'type', 'axes'));
     
     prs = sig_con(any(sig_con==allcel(i),2),:);
-    ha = tight_subplot(ceil(sqrt(1+size(prs,1))),ceil(sqrt(1+size(prs,1))),[.02 .01]);
+    [plotRows,~]= numSubplots(max(2+size(prs,1),4));
+    ha = tight_subplot(plotRows(1),plotRows(2),[.02 .01]);
+    prs2 = [];
     for j=1:length(ha)
         axes(ha(j))
         if j<=size(prs,1)
@@ -31,7 +41,7 @@ for i=1:length(allcel)
             if prs1(1)~=allcel(i)
                 prs1 = fliplr(prs1);
             end
-            
+            prs2(j,:) = prs1;
             exc=ccgR(:,prs1(1),prs1(2));
             exc(exc<Bounds(:,prs1(1),prs1(2),1)|~window)=0;
             
@@ -42,11 +52,11 @@ for i=1:length(allcel)
             hold on;
             
             % Plot predicted values
-            plot(t,Pred(:,prs1(1),prs1(2)),'g');
+            plot(t,Pred(:,prs1(1),prs1(2)),'g', 'HitTest','off');
             
             %Plot upper and lower boundaries
-            plot(t,Bounds(:,prs1(1),prs1(2),1),'r--');
-            plot(t,Bounds(:,prs1(1),prs1(2),2),'r--');
+            plot(t,Bounds(:,prs1(1),prs1(2),1),'r--', 'HitTest','off');
+            plot(t,Bounds(:,prs1(1),prs1(2),2),'r--', 'HitTest','off');
             
             % Plot signif increased bins in red
             bar_from_patch(t,exc,'r')
@@ -57,7 +67,7 @@ for i=1:length(allcel)
             % bar(t,inh,1,'FaceColor','c','EdgeColor','c');
             
             upL = get(gca,'ylim');
-            plot([0 0],[0 upL(2)],'k')
+            plot([0 0],[0 upL(2)],'k', 'HitTest','off')
             xlim([min(t) max(t)]);
             
             text(min(t) +.1*abs(min(t)),double(max(ccgR(:,prs(j,1),prs(j,2)))),['max cnt: ' num2str(max(ccgR(:,prs(j,1),prs(j,2))))])
@@ -83,21 +93,30 @@ for i=1:length(allcel)
             axhpos = get(axh,'Position');
             set(axh,'Position',[axhpos(1) axhpos(2)-axhpos(4)*.2 axhpos(3) axhpos(4)],'XTickLabel',[]);
             
-        elseif j<length(ha)
+        elseif j<length(ha)-1
             axis off
+        elseif j<length(ha)
+            zdata = ccgR(:,:,allcel(i))';
+            imagesc(flip(t),1:size(ccgR,3),(zdata'./max(zdata'))'), hold on
+            plot(-0.058*ones(size(prs2,1),1),prs2(:,2),'.w', 'HitTest','off', 'MarkerSize',12)
+            plot(-0.058*ones(size(prs2,1),1),prs2(:,2),'ok', 'HitTest','off')
+            plot(-0.058*ones(size(prs2,1),1),prs2(:,1),'.k', 'HitTest','off', 'MarkerSize',12)
+            plot(-0.058*ones(size(prs2,1),1),prs2(:,1),'ok', 'HitTest','off')
         else
             bar_from_patch(t,ccgR(:,allcel(i),allcel(i)),'k')
             % bar(t,ccgR(:,allcel(i),allcel(i)),1,'FaceColor','k','EdgeColor','k');
             xlim([min(t) max(t)]);
             xlabel('Reference Cell ACG');
             targ=completeIndex(completeIndex(:,3) == allcel(i),1:2);
-            mtit(['Reference Cell sh: ' num2str(targ(1)) ' cell '  num2str(targ(2)),' (', num2str(i),'/' num2str(length(allcel)),')'])
+            plotTitle.String = ['Reference Cell sh: ' num2str(targ(1)) ' cell '  num2str(targ(2)),' (', num2str(i),'/' num2str(length(allcel)),')'];
+            %             mtit(['Reference Cell sh: ' num2str(targ(1)) ' cell '  num2str(targ(2)),' (', num2str(i),'/' num2str(length(allcel)),')'])
             
             uiwait(h);
         end
     end
     
 end
+
 if ishandle(h)
     close(h)
 end
@@ -115,14 +134,31 @@ end
         end
     end
 
-    function advance
+    function goBack
+        i = max(i-1,1);
         uiresume(h);
+    end
+
+    function advance
+        if i==length(allcel)
+            answer = questdlg('All cells have been currated. Do you want to quit?', 'Monosyn curration complete', 'Yes','No','Yes');
+            if strcmp(answer,'Yes')
+                close(h)
+            end
+        else
+            i = i+1;
+            uiresume(h);
+        end
     end
 
     function keyPress(src, e)
         switch e.Key
             case 'space'
-                uiresume(h);
+                advance
+            case 'rightarrow'
+                advance
+            case 'leftarrow'
+                goBack
         end
     end
 end
@@ -143,7 +179,7 @@ function axh = AxesInsetBars(h,ratio,color,xdata,ydata)
 
 figpos = get(h,'Position');
 newpos = [figpos(1)+(1-ratio)*figpos(3) figpos(2)+(1-ratio)*figpos(4) ratio*figpos(3) ratio*figpos(4)];
-axh = axes('Position',newpos);
+axh = axes('Position',newpos, 'HitTest','off');
 
 bar_from_patch(xdata,ydata,color)
 % bar(xdata,ydata,1,'FaceColor',color,'EdgeColor',color)
@@ -152,11 +188,44 @@ end
 
 function bar_from_patch(x_data, y_data,col)
 % Creates a bar graph using the patch plot mode, which is substantial
-% faster than using the regular bar plot. 
+% faster than using the regular bar plot.
 % By Peter Petersen
 
 x_step = x_data(2)-x_data(1);
 x_data = [x_data(1),reshape([x_data,x_data+x_step]',1,[]),x_data(end)];
 y_data = [0,reshape([y_data,y_data]',1,[]),0];
-patch(x_data, y_data,col,'EdgeColor',col)
+patch(x_data, y_data,col,'EdgeColor',col, 'HitTest','off')
+end
+
+function [p,n]=numSubplots(n)
+% Calculate how many rows and columns of sub-plots are needed to
+% neatly display n subplots.
+% Rob Campbell - January 2010
+
+while isprime(n) && n>4
+    n=n+1;
+end
+p=factor(n);
+if length(p)==1
+    p=[1,p];
+    return
+end
+while length(p)>2
+    if length(p)>=4
+        p(1)=p(1)*p(end-1);
+        p(2)=p(2)*p(end);
+        p(end-1:end)=[];
+    else
+        p(1)=p(1)*p(2);
+        p(2)=[];
+    end
+    p=sort(p);
+end
+
+%Reformat if the column/row ratio is too large: we want a roughly
+%square design
+while p(2)/p(1)>2.5
+    N=n+1;
+    [p,n]=numSubplots(N); %Recursive!
+end
 end
